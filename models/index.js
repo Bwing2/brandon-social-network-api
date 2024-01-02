09b00ccd77d0f55d74fc5860d4f7551a375202fc
Brandon Wing <brandon.wing245@gmail.com>
2024-01-02 12:47:58 -0700

    Added Thought and User model. Also created Reaction schema.

---
+++ models/index.js
@@ -0,0 +1,6 @@
+// Imports 3 modules to export as an object
+const Reaction = require('./Reaction');
+const Thought = require('./Thought');
+const User = require('./User');
+
+module.exports = { Reaction, Thought, User };
